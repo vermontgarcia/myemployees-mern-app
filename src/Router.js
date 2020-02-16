6c7dfec190297f68df0d1c13bc9c1809d2848c96
Vermont Garcia <vermontgarcia@hotmail.com>
2020-02-16 02:46:40 -0600

    Login route added

--- src/Router.js
+++ src/Router.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Switch, Route} from 'react-router-dom'
 import Home from './Components/Home';
+import Login from './Components/Login';
 
 
 const Router = () => (
@@ -8,8 +9,13 @@ const Router = () => (
     <Route exact path='/' render={(props) => (
       <Home
         {...props}
-    />)}
-  />
+      />)}
+    />
+    <Route exact path='/login' render={(props) => (
+      <Login
+        {...props} 
+      />)}
+    />
   </Switch>
 );
 
