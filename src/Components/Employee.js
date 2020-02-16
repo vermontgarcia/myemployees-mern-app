55a75d50da1ddda5ce2e7a6d685a627dbf9291a9
Vermont Garcia <vermontgarcia@hotmail.com>
2020-02-16 05:27:22 -0600

    Employee card

--- src/Components/Employee.js
+++ src/Components/Employee.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { Card } from 'antd';
 
+const {Meta} = Card;
 
-const Employee = () => (
+const Employee = ({employee}) => (
   <div>
-    Employee Card
+    <Card
+      hoverable
+      className="employee-card"
+      cover={<img alt="Test" src={employee.profilePicture === 'avatar' ? '/avatar.png' : employee.profilePicture} />}
+    >
+      <Meta
+        title={employee.name}
+        description={employee.position}
+      />
+
+    </Card>
   </div>
 )
 
