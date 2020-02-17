d1d27d235ae543b9642de52f621cca2d61f8f0af
Vermont Garcia <vermontgarcia@hotmail.com>
2020-02-17 13:12:48 -0600

    Showing Log message confirming recorded to database

--- src/Services/logService.js
+++ src/Services/logService.js
@@ -1,10 +1,14 @@
 import axios from 'axios';
+import {message} from 'antd'
 
 const base_url = window.location.hostname === 'localhost' ? 'http://localhost:3500/api' : 'https://my-employees-2020.herokuapp.com/api';
 
 // Log services
 export const writeLog = (log) => {
   axios.post(`${base_url}/log`, log)
+    .then(res=>{
+      message.success(`${res.data.msg}, Type: "${res.data.log.logName}" at "${Date(res.data.log.created_at)}"` , 5);
+    })
     .catch(err=>{
       console.log('Log Error =====>', err.response.data.msg);
     });
